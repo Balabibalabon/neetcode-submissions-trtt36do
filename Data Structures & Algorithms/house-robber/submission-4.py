9a55690b7c4f22909162e3714525ccfaa362b2a8
Heng-Syu Lin <127748303+Balabibalabon@users.noreply.github.com>
2026-04-07 10:06:03 +0800

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 2:
+            return max(nums)
+        
+        prev_2 = nums[0]
+        prev_1 = max(nums[0],nums[1])
+        for i in range(2,n):
+            cur = nums[i] + prev_2
+            res = max(prev_1, cur)
+            prev_2 = prev_1
+            prev_1 = res
+            # print("現在在看第",i+1,"格，目前兩格狀態為:",prev_2, prev_1)
+        return res
